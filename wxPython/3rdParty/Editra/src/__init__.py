a22352878f87ac781470ef5998b65b17ea45250f
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2007-10-20 02:40:49 +0000

    Initial import of Editra v0.2.x code

---
+++ wxPython/3rdParty/Editra/src/__init__.py
@@ -0,0 +1,19 @@
+###############################################################################
+# Name: __init__.py                                                           #
+# Purpose: Import the required base modules needed for launching Editra into  #
+#          into the namespace.                                                #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2007 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
+"""Main package module
+@summary: Makes ed_glob, util, and Editra available in the package namespace
+
+"""
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id$"
+__revision__ = "$Revision$"
+
+import ed_glob
+import util
+import Editra
