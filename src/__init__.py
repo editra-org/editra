828663b3075020cb376444eed2eb66aa856ae214
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2007-11-10 07:08:33 +0000

    avoid needing to launch two instances of python when using toplevel launch script for running program

--- src/__init__.py
+++ src/__init__.py
@@ -6,14 +6,8 @@
 # Copyright: (c) 2007 Cody Precord <staff@editra.org>                         #
 # Licence: wxWindows Licence                                                  #
 ###############################################################################
-"""Main package module
-@summary: Makes ed_glob, util, and Editra available in the package namespace
+"""Main package module"""
 
-"""
 __author__ = "Cody Precord <cprecord@editra.org>"
 __svnid__ = "$Id$"
 __revision__ = "$Revision$"
-
-import ed_glob
-import util
-import Editra
