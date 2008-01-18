8b7b2468ec812a5b60c8a2a4ee56cc7c8dcf9b93
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-01-18 09:33:44 +0000

    Add Clear button to LogViewer

--- src/ed_log.py
+++ src/ed_log.py
@@ -7,6 +7,11 @@
 ###############################################################################
 
 """
+Editra LogViewer
+
+This module provides classes for managing the log display and filtering of its
+messages. The module also exports an implementation of a shelf plugin for
+displaying a LogViewer in the Shelf.
 
 """
 
@@ -20,8 +25,11 @@ import os
 import wx
 import ed_msg
 import eclib.outbuff as outbuff
+import eclib.ctrlbox as ctrlbox
+import eclib.platebtn as platebtn
 import iface
 import plugin
+import ed_glob
 
 #-----------------------------------------------------------------------------#
 # Globals
@@ -49,6 +57,10 @@ class EdLogViewer(plugin.Plugin):
         return self.ID_LOGGER
 
     def GetMenuEntry(self, menu):
+        """Get the menu entry for the log viewer
+        @param menu: the menu items parent menu
+
+        """
         return wx.MenuItem(menu, self.ID_LOGGER, _(self.__name__), 
                            _("View Editra's console log"))
 
@@ -63,7 +75,54 @@ class EdLogViewer(plugin.Plugin):
 #-----------------------------------------------------------------------------#
 
 # LogViewer Ui Implementation
-class LogViewer(outbuff.OutputBuffer):
+class LogViewer(ctrlbox.ControlBox):
+    """LogViewer is a control for displaying and working with output
+    from Editra's log.
+    """
+
+    def __init__(self, parent):
+        ctrlbox.ControlBox.__init__(self, parent)
+
+        # Attributes
+        self._buffer = LogBuffer(self)
+        self.SetWindow(self._buffer)
+
+        # Layout
+        self.__DoLayout()
+
+        # Event Handlers
+        self.Bind(wx.EVT_BUTTON, self.OnButton)
+
+    def __DoLayout(self):
+        """Layout the log viewer window"""
+        # Setup ControlBar
+        ctrlbar = ctrlbox.ControlBar(self, style=ctrlbox.CTRLBAR_STYLE_GRADIENT)
+        if wx.Platform == '__WXGTK__':
+            ctrlbar.SetWindowStyle(ctrlbox.CTRLBAR_STYLE_DEFAULT)
+
+        ctrlbar.AddStretchSpacer()
+        cbmp = wx.ArtProvider.GetBitmap(str(ed_glob.ID_DELETE), wx.ART_MENU)
+        if cbmp.IsNull() or not cbmp.IsOk():
+            cbmp = None
+        clear = platebtn.PlateButton(ctrlbar, wx.ID_CLEAR, _("Clear"),
+                                     cbmp, style=platebtn.PB_STYLE_NOBG)
+        ctrlbar.AddControl(clear, wx.ALIGN_RIGHT)
+        ctrlbar.SetVMargin(1, 1)
+        self.SetControlBar(ctrlbar)
+        
+    def OnButton(self, evt):
+        """Handle button events from the controlbar"""
+        e_id = evt.GetId()
+        if e_id == wx.ID_CLEAR:
+            self._buffer.Clear()
+        else:
+            evt.Skip()
+
+class LogBuffer(outbuff.OutputBuffer):
+    """Buffer for displaying log messages that are sent on Editra's
+    log channel.
+
+    """
     def __init__(self, parent):
         outbuff.OutputBuffer.__init__(self, parent)
 
@@ -72,6 +131,11 @@ class LogViewer(outbuff.OutputBuffer):
 
         # Subscribe to Editra's Log
         ed_msg.Subscribe(self.UpdateLog, ed_msg.EDMSG_LOG_ALL) 
+
+    def __del__(self):
+        """Unregister from recieving any more log messages"""
+        ed_msg.Unsubscribe(self.UpdateLog, ed_msg.EDMSG_LOG_ALL)
+        super(LogBuffer, self).__del__()
 
     def UpdateLog(self, msg):
         """Add a new log message
@@ -84,5 +148,18 @@ class LogViewer(outbuff.OutputBuffer):
 
         self.AppendUpdate(str(msg.GetData()) + os.linesep)
 
-
 #-----------------------------------------------------------------------------#
+
+class LogFilterCache(dict):
+    """Data storage class for filtering and organizing log messages"""
+    def __init__(self):
+        """Create a new log cache"""
+        dict.__init__(self)
+
+    def Clear(self, subkey=None):
+        """Clear the log cache, if subkey is specified only clear
+        the subset of the cache pertaining to that
+        @keyword subkey: Toplevel cache filter to clear
+
+        """
+        
