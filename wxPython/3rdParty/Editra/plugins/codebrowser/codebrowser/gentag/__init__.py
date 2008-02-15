979dadea961ad2815c30c099a32e3c634154d117
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-02-15 23:15:07 +0000

    rename plugin

---
+++ wxPython/3rdParty/Editra/plugins/codebrowser/codebrowser/gentag/__init__.py
@@ -0,0 +1,7 @@
+###############################################################################
+# Name: __ini__.py                                                            #
+# Purpose: Tag Generator Library                                              #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2008 Cody Precord <staff@editra.org>                         #
+# License: wxWindows License                                                  #
+###############################################################################
