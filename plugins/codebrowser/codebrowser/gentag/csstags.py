5f74299c422b8bd2642a43a3d7fc5cd617800ff4
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-02-18 12:25:41 +0000

    Add support for parsing CSS, (global classes and identities)

---
+++ plugins/codebrowser/codebrowser/gentag/csstags.py
@@ -0,0 +1,123 @@
+###############################################################################
+# Name: csstags.py                                                            #
+# Purpose: Generate Tags for Cascading Style Sheets                           #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2008 Cody Precord <staff@editra.org>                         #
+# License: wxWindows License                                                  #
+###############################################################################
+
+"""
+FILE: csstags.py
+AUTHOR: Cody Precord
+LANGUAGE: Python
+SUMMARY:
+  Generate a DocStruct object that captures the structure of a Cascading Style
+Sheet. Currently supports parsing of global identities and classes.
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id$"
+__revision__ = "$Revision$"
+
+#--------------------------------------------------------------------------#
+# Dependancies
+import taglib
+import parselib
+
+#--------------------------------------------------------------------------#
+
+def GenerateTags(buff):
+    """Create a DocStruct object that represents a Cascading Style Sheets
+    @param buff: a file like buffer object (StringIO)
+    @todo: add support for parsing selectors and grouping classes and
+           identities of each selector in a subscope.
+
+    """
+    rtags = taglib.DocStruct()
+
+    # Setup document structure
+#    rtags.SetElementDescription('tag', "Selectors")
+    # Use variables node for global identities
+    rtags.SetElementDescription('variable', "Identities")
+    # Use classes for global classes
+    # Uses DocStruct builtin
+
+    c_tag = None      # Currenly found tag
+    incomment = False # Inside a comment
+    indef = False     # Inside a style definition {}
+
+    for lnum, line in enumerate(buff):
+        line = line.strip()
+        llen = len(line)
+        idx = 0
+        while idx < len(line):
+            # Skip Whitespace
+            while line[idx].isspace():
+                idx += 1
+
+            # Check if valid item to add to document
+#            if c_tag is not None and line[idx] == u'{':
+#                rtags.AddElement('tag', c_tag)
+#                c_tag = None
+
+            # Check for coments
+            if line[idx] == u'/' and llen > idx and line[idx+1] == u'*':
+                idx += 1
+                incomment = True
+            elif line[idx] == u'*' and llen > idx and line[idx+1] == u'/':
+                idx += 1
+                incomment = False
+
+            # Look for tags
+            if incomment:
+                idx += 1
+            elif line[idx] == u'{':
+                idx += 1
+                indef = True
+            elif indef and line[idx] == u'}':
+                idx += 1
+                indef = False
+            elif not indef and line[idx] in (u'.', u'#'):
+                if idx == 0 or line[idx-1].isspace():
+                    name = parselib.GetFirstIdentifier(line[idx+1:])
+                    if name is not None:
+                        name = line[idx] + name
+                        if line[idx] == u'.':
+                            rtags.AddClass(taglib.Class(name, lnum))
+                        else:
+                            rtags.AddVariable(taglib.Variable(name, lnum))
+#                        idx += (len(name) + 1)
+                    idx += 1
+                else:
+                    idx += 1
+            else:
+                idx += 1
+
+    return rtags
+
+#-----------------------------------------------------------------------------#
+# Utilities
+class Tag(taglib.Scope):
+    """Tag Scope Object, Tag derives from Scope so it can
+    contain identities and classes
+
+    """
+    def __init__(self, name, line, scope=None):
+        taglib.Scope.__init__(self, name, line, "tag", scope)
+
+#-----------------------------------------------------------------------------#
+# Test
+if __name__ == '__main__':
+    import sys
+    import StringIO
+    fhandle = open(sys.argv[1])
+    txt = fhandle.read()
+    fhandle.close()
+    tags = GenerateTags(StringIO.StringIO(txt))
+    print "\n\nElements:"
+    for element in tags.GetElements():
+        print "\n%s:" % element.keys()[0]
+        for val in element.values()[0]:
+            print "%s [%d]" % (val.GetName(), val.GetLine())
+    print "END"
