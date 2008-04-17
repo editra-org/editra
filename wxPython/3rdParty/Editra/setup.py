777bf68dea9bb3edd2662be778b446b7bf1bb85e
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-04-17 23:20:26 +0000

    Add Editra.pyw launcher file

--- wxPython/3rdParty/Editra/setup.py
+++ wxPython/3rdParty/Editra/setup.py
@@ -11,7 +11,7 @@
  Editra Setup Script
 
  USAGE:
- 
+
    1) Windows:
       - python setup.py py2exe
 
@@ -80,7 +80,7 @@ SRC_SCRIPTS = [ ("src", glob.glob("src/*.py")),
                 ("scripts/i18n", glob.glob("scripts/i18n/*.po")),
 ]
 
-DATA_FILES = [("include/python2.5", 
+DATA_FILES = [("include/python2.5",
                glob.glob("include/python2.5/%s/*" % __platform__)),
               ("pixmaps", ["pixmaps/editra.png", "pixmaps/editra.ico",
                            "pixmaps/editra.icns", "pixmaps/editra_doc.icns",
@@ -88,26 +88,32 @@ DATA_FILES = [("include/python2.5",
               ("pixmaps/theme/Default", ["pixmaps/theme/Default/README"]),
                ("pixmaps/theme/Tango",["pixmaps/theme/Tango/AUTHORS",
                                       "pixmaps/theme/Tango/COPYING"]),
-              ("pixmaps/theme/Tango/toolbar", 
+              ("pixmaps/theme/Tango/toolbar",
                glob.glob("pixmaps/theme/Tango/toolbar/*.png")),
-              ("pixmaps/theme/Tango/menu", 
+              ("pixmaps/theme/Tango/menu",
                glob.glob("pixmaps/theme/Tango/menu/*.png")),
-              ("pixmaps/theme/Tango/mime", 
+              ("pixmaps/theme/Tango/mime",
                glob.glob("pixmaps/theme/Tango/mime/*.png")),
               ("plugins", glob.glob("plugins/*.egg")),
               ("templates", glob.glob("templates/*")),
               ("locale/de_DE/LC_MESSAGES",
                ["locale/de_DE/LC_MESSAGES/Editra.mo"]),
-              ("locale/en_US/LC_MESSAGES", 
+              ("locale/en_US/LC_MESSAGES",
                ["locale/en_US/LC_MESSAGES/Editra.mo"]),
-              ("locale/es_ES/LC_MESSAGES", 
+              ("locale/es_ES/LC_MESSAGES",
                ["locale/es_ES/LC_MESSAGES/Editra.mo"]),
-              ("locale/ja_JP/LC_MESSAGES", 
+              ("locale/it_IT/LC_MESSAGES",
+               ["locale/it_IT/LC_MESSAGES/Editra.mo"]),
+              ("locale/ja_JP/LC_MESSAGES",
                ["locale/ja_JP/LC_MESSAGES/Editra.mo"]),
-              ("locale/nl_NL/LC_MESSAGES", 
+              ("locale/nl_NL/LC_MESSAGES",
                ["locale/nl_NL/LC_MESSAGES/Editra.mo"]),
               ("locale/ru_RU/LC_MESSAGES",
                ["locale/ru_RU/LC_MESSAGES/Editra.mo"]),
+              ("locale/sr_YU/LC_MESSAGES",
+               ["locale/sr_YU/LC_MESSAGES/Editra.mo"]),
+              ("locale/tr_TR/LC_MESSAGES",
+               ["locale/tr_TR/LC_MESSAGES/Editra.mo"]),
               ("locale/zh_CN/LC_MESSAGES",
                ["locale/zh_CN/LC_MESSAGES/Editra.mo"]),
               ("styles", glob.glob("styles/*.ess")),
@@ -119,21 +125,24 @@ DATA_FILES = [("include/python2.5",
              ]
 
 DATA = [ "src/*.py", "src/syntax/*.py", "src/autocomp/*.py", "src/eclib/*.py",
-         "docs/*.txt", "pixmaps/*.png", "pixmaps/*.ico", 'Editra', 
-         "src/extern/*.py", "pixmaps/*.icns", "pixmaps/theme/Default/README", 
+         "docs/*.txt", "pixmaps/*.png", "pixmaps/*.ico", 'Editra',
+         "src/extern/*.py", "pixmaps/*.icns", "pixmaps/theme/Default/README",
          "pixmaps/theme/Tango/AUTHOR", "pixmaps/theme/Tango/COPYING",
          "pixmaps/theme/Tango/toolbar/*.png", "pixmaps/theme/Tango/menu/*.png",
          "pixmaps/theme/Tango/mime/*.png", "pixmaps/theme/Default/README",
          "locale/de_DE/LC_MESSAGES/Editra.mo",
          "locale/en_US/LC_MESSAGES/Editra.mo",
-         "locale/es_ES/LC_MESSAGES/Editra.mo", 
-         "locale/ja_JP/LC_MESSAGES/Editra.mo", 
-         "locale/nl_NL/LC_MESSAGES/Editra.mo", 
-         "locale/ru_RU/LC_MESSAGES/Editra.mo", 
-         "locale/zh_CN/LC_MESSAGES/Editra.mo", 
-#          "tests/controls/*", 
+         "locale/es_ES/LC_MESSAGES/Editra.mo",
+         "locale/it_IT/LC_MESSAGES/Editra.mo",
+         "locale/ja_JP/LC_MESSAGES/Editra.mo",
+         "locale/nl_NL/LC_MESSAGES/Editra.mo",
+         "locale/ru_RU/LC_MESSAGES/Editra.mo",
+         "locale/sr_YU/LC_MESSAGES/Editra.mo",
+         "locale/tr_TR/LC_MESSAGES/Editra.mo",
+         "locale/zh_CN/LC_MESSAGES/Editra.mo",
+#          "tests/controls/*",
          "styles/*.ess", "tests/syntax/*",
-         "AUTHORS", "CHANGELOG","COPYING", "FAQ", "INSTALL", "NEWS", "README", 
+         "AUTHORS", "CHANGELOG","COPYING", "FAQ", "INSTALL", "NEWS", "README",
          "THANKS", "TODO", "setup.cfg", "plugins/*.egg"
 ]
 
@@ -144,9 +153,9 @@ r"""
 ========
 Overview
 ========
-Editra is a multi-platform text editor with an implementation that focuses on 
-creating an easy to use interface and features that aid in code development. 
-Currently it supports syntax highlighting and variety of other useful features 
+Editra is a multi-platform text editor with an implementation that focuses on
+creating an easy to use interface and features that aid in code development.
+Currently it supports syntax highlighting and variety of other useful features
 for over 60 programing languages. For a more complete list of features and
 screenshots visit the projects homepage at `Editra.org
 <http://www.editra.org/>`_.
@@ -222,15 +231,15 @@ if __platform__ == "win32" and 'py2exe' in sys.argv:
     sys.path.append(os.path.abspath('src/'))
 
     setup(
-        name = NAME, 
-        version = VERSION, 
+        name = NAME,
+        version = VERSION,
         options = {"py2exe" : {"compressed" : 1,
                                "optimize" : 2,
                                "bundle_files" : 2,
                                "includes" : INCLUDES }},
         windows = [{"script": "src/Editra.py",
-                    "icon_resources": [(0, ICON['Win'])], 
-                    "other_resources" : [(RT_MANIFEST, 1, 
+                    "icon_resources": [(0, ICON['Win'])],
+                    "other_resources" : [(RT_MANIFEST, 1,
                                           MANIFEST_TEMPLATE % dict(prog=NAME))],
                   }],
         description = DESCRIPTION,
@@ -264,8 +273,8 @@ elif __platform__ == "darwin" and 'py2app' in sys.argv:
        #      NSAppleScriptEnabled="YES",
              NSHumanReadableCopyright = u"Copyright %s 2005-%d" % (AUTHOR, YEAR)
              )
- 
-    PY2APP_OPTS = dict(iconfile = ICON['Mac'], 
+
+    PY2APP_OPTS = dict(iconfile = ICON['Mac'],
                        argv_emulation = True,
                        optimize = True,
                        includes = INCLUDES,
@@ -273,7 +282,7 @@ elif __platform__ == "darwin" and 'py2app' in sys.argv:
 
     setup(
         app = APP,
-        version = VERSION, 
+        version = VERSION,
         options = dict( py2app = PY2APP_OPTS),
         description = DESCRIPTION,
         author = AUTHOR,
@@ -302,7 +311,7 @@ else:
 
     setup(
         name = NAME,
-        scripts = ['Editra'],
+        scripts = ['Editra', 'Editra.pyw'],
         version = VERSION,
         description = DESCRIPTION,
         long_description = LONG_DESCRIPT,
