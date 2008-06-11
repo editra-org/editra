d16412a23b32ba12b21798da67c2bfdba362c3b0
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-06-11 01:17:58 +0000

    Improve accuracy Perl DocStruct generator

--- plugins/codebrowser/codebrowser/gentag/perltags.py
+++ plugins/codebrowser/codebrowser/gentag/perltags.py
@@ -15,6 +15,12 @@ SUMMARY:
 Currently supports parsing of subroutines and their declarations, as well as
 package declarations.
 
+Subroutine Declarations can be in a number of forms
+sub foo;
+sub foo :attr;
+sub foo (bar);
+sub foo (bar) :attr;
+
 """
 
 __author__ = "Cody Precord <cprecord@editra.org>"
@@ -26,6 +32,78 @@ __revision__ = "$Revision$"
 import taglib
 import parselib
 
+from pygments import highlight
+from pygments.token import Token
+from pygments.lexers import get_lexer_by_name
+from pygments.formatter import Formatter
+
+#--------------------------------------------------------------------------#
+
+class PerlFormatter(Formatter):
+    
+    def format(self, tokensource, outfile):
+        """Format the input text
+        @note: overrides Formatter.format
+
+        """
+        self.rtags = taglib.DocStruct()
+        self.rtags.SetElementDescription('package', "Packages")
+        self.rtags.SetElementPriority('package', 3)
+        self.rtags.SetElementDescription('subdec', "Subroutine Declarations")
+        self.rtags.SetElementPriority('subdec', 2)
+        self.rtags.SetElementDescription('subroutine', "Subroutines")
+        self.rtags.SetElementPriority('subroutine', 1)
+
+        line_count = 0
+        current_line = []
+        code_lines = []
+
+        # Parse the file into tokens and values
+        for ttype, value in tokensource:
+            if '\n' in value:
+                code_lines.append((line_count, current_line))
+                current_line = []
+                line_count += value.count('\n')
+                continue
+            current_line.append((ttype, value))
+
+        self.parseTags(code_lines)
+
+    def parseTags(self, code_lines):
+        """Parse all the tokens found in the lines of code"""
+        container_list = []
+        vset = set()
+
+        for num, line in code_lines:
+            try:
+                # Subroutine
+                if parselib.HasToken(line, Token.Keyword, "sub"):
+                    fname = parselib.GetTokenValue(line, Token.Name.Function)
+                    self.rtags.AddElement('subroutine',
+                                          taglib.Function(fname, num, "subroutine"))
+
+                # Packages
+                if parselib.HasToken(line, Token.Name.Builtin, "package"):
+                    name = None
+                    next = True
+                    for token, value in line:
+                        if not next:
+                            name += value
+                            break
+
+                        if token == Token.Name.Namespace:
+                            name = value
+                            next = False
+
+                    if name is not None:
+                        self.rtags.AddElement('package', taglib.Package(name, num))
+
+            except parselib.TokenNotFound:
+                pass
+
+    def getTags(self):
+        return self.rtags
+
 #--------------------------------------------------------------------------#
 
 def GenerateTags(buff):
@@ -33,108 +111,12 @@ def GenerateTags(buff):
     @param buff: a file like buffer object (StringIO)
 
     """
-    rtags = taglib.DocStruct()
-    rtags.SetElementDescription('package', "Packages")
-    rtags.SetElementPriority('package', 3)
-    rtags.SetElementDescription('subdec', "Subroutine Declarations")
-    rtags.SetElementPriority('subdec', 2)
-    rtags.SetElementDescription('subroutine', "Subroutines")
-    rtags.SetElementPriority('subroutine', 1)
-    inpod = False
-
-    for lnum, line in enumerate(buff):
-        # Check for POD docs and skip as necessary
-        if line.startswith(u"=") and len(line) > 2:
-            if line.startswith(u"=cut"):
-                inpod = False
-            elif line[1].isalpha():
-                inpod = True
-            continue
-
-        if inpod:
-            continue
-
-        # Not in POD so try to parse for elements
-        line = line.strip()
-        llen = len(line)
-
-        # Skip comment and empty lines
-        if line.startswith(u"#") or not line:
-            continue
-
-        # Check for subroutines
-        if parselib.IsToken(line, 0, u'sub'):
-            sub = ExtractSubroutine(line)
-            if sub is not None:
-                if sub[0]:
-                    rtags.AddElement('subdec',
-                                     taglib.Function(sub[1], lnum, "subdec"))
-                else:
-                    rtags.AddElement('subroutine',
-                                     taglib.Function(sub[1], lnum, "subroutine"))
-        elif parselib.IsToken(line, 0, u'package'):
-            # Look for a package declaration
-            parts = line.split()
-            if line.endswith(u";") and len(parts) <= 3:
-                name = parts[1].rstrip(u";")
-                rtags.AddElement('package', taglib.Package(name, lnum))
-        else:
-            pass
-
-    return rtags
+    formatter = PerlFormatter()
+    highlight(buff.read(), get_lexer_by_name("perl"), formatter)
+    return formatter.getTags()
 
 #-----------------------------------------------------------------------------#
-# Utilities
 
-def ExtractSubroutine(line):
-    """Extract a subroutine defintion from a line, if no valid
-    subroutine is found in the line it will return None. It is
-    assumed that passed in string starts with 'sub'.
-    @param line: string
-    @return: tuple (isdecl, name) or None
-
-    """
-    line = line[3:].strip()
-    sub = ''
-    foundname = False
-    isdecl = False
-
-    for char in line:
-        if char.isalnum() or char == u"_":
-            if not foundname:
-                sub += char
-            else:
-                # If the name was already found this is invalid
-                break
-        elif char in [u";", u":", u"("] and line.endswith(u";"):
-            # Declarations can be in a number of forms
-            # sub foo;
-            # sub foo :attr;
-            # sub foo (bar);
-            # sub foo (bar) :attr;
-            foundname = True
-            isdecl = True
-            break
-        elif char == u"{":
-            foundname = True
-            isdecl = False
-            break
-        elif char.isspace():
-            # Assume first space found is end of name
-            # keep checking to see if its a declaration or not
-            foundname = True
-        else:
-            return None
-    else:
-        # Reached end of line assume { is on next line
-        isdecl = False
-
-    if len(sub) and foundname:
-        return (isdecl, sub)
-    else:
-        return None
-
-#-----------------------------------------------------------------------------#
 # Test
 if __name__ == '__main__':
     import sys
