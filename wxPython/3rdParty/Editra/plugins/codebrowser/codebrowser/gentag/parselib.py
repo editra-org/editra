45bc771b965ecabb780a25c527edf5b3e2862838
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-06-11 01:17:58 +0000

    Improve accuracy Perl DocStruct generator

--- wxPython/3rdParty/Editra/plugins/codebrowser/codebrowser/gentag/parselib.py
+++ wxPython/3rdParty/Editra/plugins/codebrowser/codebrowser/gentag/parselib.py
@@ -30,7 +30,11 @@ class TokenNotFound(Exception):
 # Helper functions for modules using pygments
 
 def GetTokenValue(line, searchToken):
-    """Get the value associated with the given token type in the line"""
+    """Get the value associated with the given token type in the line
+    will raise TokenNotFound if the value cannot be found
+    @return: string
+
+    """
     for token, value in line:
         if token == searchToken:
             return value
