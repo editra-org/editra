ae603db790d3b9e99c8e756b4683111357d8c214
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-07-06 18:46:43 +0000

    more missing keywords

--- wxPython/3rdParty/Editra/src/syntax/ruby.py
+++ wxPython/3rdParty/Editra/src/syntax/ruby.py
@@ -32,7 +32,7 @@ RUBY_KW = (0, "__FILE__ and def end in or self unless __LINE__ begin defined "
               "ensure module redo super until BEGIN break do false next "
               "require rescue then when END case else for nil retry true while "
               "alias class elsif if not return undef yieldr puts raise "
-              "protected")
+              "protected private")
 
 #---- Syntax Style Specs ----#
 SYNTAX_ITEMS = [ ('STC_RB_BACKTICKS', 'scalar_style'),
