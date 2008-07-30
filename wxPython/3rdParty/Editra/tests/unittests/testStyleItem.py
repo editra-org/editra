47d615ab5cc495b16f9f8b8f747f21bd40719515
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-07-30 21:24:23 +0000

    add unittest for testing the StyleItem class

---
+++ wxPython/3rdParty/Editra/tests/unittests/testStyleItem.py
@@ -0,0 +1,110 @@
+###############################################################################
+# Name: testStyleItem                                                         #
+# Purpose: Unittest for ed_style.StyleItem                                    #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2008 Cody Precord <staff@editra.org>                         #
+# License: wxWindows License                                                  #
+###############################################################################
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id$"
+__revision__ = "$Revision$"
+
+#-----------------------------------------------------------------------------#
+# Imports
+import os
+import unittest
+
+# Module to test
+import sys
+sys.path.insert(0, os.path.abspath("../../src"))
+import ed_style
+
+#-----------------------------------------------------------------------------#
+# Test Class
+
+class StyleItemTest(unittest.TestCase):
+    def setUp(self):
+        self.item = ed_style.StyleItem("#FF0000,bold", "#000000", "Times", "10")
+        self.itemstr = "fore:#FF0000,bold,back:#000000,face:Times,size:10"
+
+    def tearDown(self):
+        pass
+
+    #---- Method Tests ----#
+    def testEquals(self):
+        """Test that the equality operator is functioning correctly"""
+        item2 = ed_style.StyleItem("#FF0000,bold", "#000000", "Times", "10")
+
+        # Base Test
+        self.assertEquals(self.item, item2)
+
+        # Test 2
+        item3 = ed_style.StyleItem("#FF0000", "#0000FF", "Arial", "10")
+        self.assertNotEquals(self.item, item3,
+                             "%s == %s" % (str(self.item), str(item2)))
+
+    def testString(self):
+        """Test that string conversion works properly"""
+        items1 = sorted(str(self.item).split(','))
+        items2 = sorted(self.itemstr.split(','))
+        self.assertEquals(items1, items2)
+
+    def testGetAsList(self):
+        """Test getting the style item attributes as a list"""
+        itemlst = self.item.GetAsList()
+        self.assertTrue(len(itemlst) == 4, "GetAsList didn't return all items")
+
+    def testGetBack(self):
+        """Test retrieving the background color"""
+        self.assertEquals(self.item.GetBack(), "#000000")
+
+    def testGetFore(self):
+        """Test retrieving the background color"""
+        self.assertEquals(self.item.GetFore(), "#FF0000")
+
+    def testGetFace(self):
+        """Test retrieving the background color"""
+        self.assertEquals(self.item.GetFace(), "Times")
+
+    def testGetSize(self):
+        """Test retrieving the background color"""
+        self.assertEquals(self.item.GetSize(), "10")
+
+    def testGetNamedAttr(self):
+        """Test GetNamedAttr"""
+        self.assertEquals(self.item.GetNamedAttr("fore"), self.item.GetFore())
+        self.assertEquals(self.item.GetNamedAttr("back"), self.item.GetBack())
+        self.assertEquals(self.item.GetNamedAttr("face"), self.item.GetFace())
+        self.assertEquals(self.item.GetNamedAttr("size"), self.item.GetSize())
+
+    def testIsNull(self):
+        """Test Null check"""
+        self.assertFalse(self.item.IsNull())
+        self.assertTrue(ed_style.NullStyleItem().IsNull())
+
+    def testIsOk(self):
+        """Test checker for if an item is Ok"""
+        self.assertTrue(self.item.IsOk())
+        self.assertFalse(ed_style.StyleItem().IsOk())
+
+    def testNullify(self):
+        """Test nullifying a style item"""
+        item = ed_style.StyleItem("#000000", "#FFFFFF")
+        self.assertFalse(item.IsNull(), "Item is already null")
+        item.Nullify()
+        self.assertTrue(item.IsNull(), "Item was not nullified")
+
+    def testSetAttrFromString(self):
+        """Test Setting attributes from a formatted string"""
+        item = ed_style.StyleItem()
+        item.SetAttrFromStr(self.itemstr)
+        self.assertEquals(self.item, item,
+                          "%s != %s" % (str(self.item), str(item)))
+
+    #TODO: add set item tests after next revision of ed_style
+#    def testSet
+#-----------------------------------------------------------------------------#
+
+if __name__ == '__main__':
+    unittest.main()
