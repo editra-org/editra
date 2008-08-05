c7e9905e48020ce0e391f18532044d112d7ae558
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-08-05 23:00:19 +0000

    Resync and update all catalogs

--- wxPython/3rdParty/Editra/scripts/i18n/gen_lang.sh
+++ wxPython/3rdParty/Editra/scripts/i18n/gen_lang.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
-# generates po and mo files from the project source code
+##############################################################################
+# Generates po and mo files from the project source code
+# Author: Cody Precord
+# Copyright: Cody Precord <cprecord@editra.org>
+# License: wxWindows
+##############################################################################
 
+##############################################################################
+# Variables
+##############################################################################
 ARG=$1
 
+##############################################################################
+# Function: print_help
+# Purpose: Print the scripts usage help to the console
+##############################################################################
 print_help () {
 	echo
-	echo "Usage: $0 [-h|-mo|-po]"
+	echo "Usage: $0 [-h|-mo|-po|-all]"
 	echo "    -h    Print this help message"
 	echo "    -mo   Generate mo files and install them in the locale directory"
 	echo "    -po   Generate new po files from the project source"
+    echo "    -all  Regenerate everything"
 	echo
 }
 
-if [ "$ARG" = "-po" ]
-then
+##############################################################################
+# Function: gen_po
+# Purpose: Generate new po files from the source
+##############################################################################
+gen_po () {
     env python2.5 mki18n.py -pv --domain=Editra
     # Copy all .new files to override the originals
     for fname in $(ls); do
@@ -22,9 +38,31 @@ then
             mv $fname $name
         fi
     done
+}
+
+##############################################################################
+# Function: make_mo
+# Purpose: Make mo files and place them in the appropriate locale directory
+##############################################################################
+make_mo () {
+    env python2.5 mki18n.py -mv --domain=Editra --moTarget=../../locale
+}
+
+
+##############################################################################
+# Main
+##############################################################################
+
+if [ "$ARG" = "-po" ]
+then
+    gen_po
 elif [ "$ARG" = "-mo" ]
 then
-    env python2.5 mki18n.py -mv --domain=Editra --moTarget=../../locale
+    make_mo
+elif [ "$ARG" = "-all" ]
+then
+    gen_po
+    make_mo
 else
     print_help
 fi    
