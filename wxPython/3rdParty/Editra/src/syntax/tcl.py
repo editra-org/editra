b5b4c35ec62499053e8121c525688233866bfa29
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-08-18 13:34:52 +0000

    fix incorrect styling of {asdf"asdf} strings.

--- wxPython/3rdParty/Editra/src/syntax/tcl.py
+++ wxPython/3rdParty/Editra/src/syntax/tcl.py
@@ -149,7 +149,7 @@ SYNTAX_ITEMS = [('STC_TCL_BLOCK_COMMENT', 'comment_style'),
                  ('STC_TCL_NUMBER', 'number_style'),
                  ('STC_TCL_OPERATOR', 'operator_style'),
                  ('STC_TCL_SUBSTITUTION', 'scalar_style'),
-                 ('STC_TCL_SUB_BRACE', 'default_style'), # STYLE NEEDED
+                 ('STC_TCL_SUB_BRACE', 'string_style'), # STYLE NEEDED
                  ('STC_TCL_WORD', 'keyword_style'),        # tcl_kw
                  ('STC_TCL_WORD2', 'keyword2_style'),      # tk_kw
                  ('STC_TCL_WORD3', 'keyword3_style'),      # itcl_kw
