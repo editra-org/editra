fb94796c2332c2c54e6345e2b7a83b8af2022820
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-12-07 00:13:00 +0000

    set executable bit

--- wxPython/3rdParty/Editra/scripts/i18n/mki18n.py
+++ wxPython/3rdParty/Editra/scripts/i18n/mki18n.py
@@ -151,7 +151,7 @@ def makePO(app_dir,  app_domain=None, verbose=0) :
     #        (easier to use when we need to merge several .po files)
     #   --files-from=app.fil : The list of files is taken from the file: app.fil
     #   --output= : specifies the name of the output file 
-    #               (using a .pot extension)
+    #               (using a .pot extension) 
     cmd = 'xgettext -s --from-code=utf-8 --no-wrap --files-from=app.fil --output=messages.pot'
     if verbose: 
         print cmd
