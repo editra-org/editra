26d2b0200450178b9b46eb5152595583a672753e
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-12-15 01:16:39 +0000

    Move PyFontPicker to separate module in eclib

--- wxPython/3rdParty/Editra/src/prefdlg.py
+++ wxPython/3rdParty/Editra/src/prefdlg.py
@@ -40,6 +40,7 @@ import util
 import syntax.syntax as syntax
 import ed_msg
 import eclib.elistmix as elistmix
+import eclib.ecpickers as ecpickers
 
 #----------------------------------------------------------------------------#
 # Globals
@@ -568,12 +569,12 @@ class DocGenPanel(wx.Panel):
         fnt = Profile_Get('FONT1', 'font', wx.Font(10, wx.FONTFAMILY_MODERN,
                                                    wx.FONTSTYLE_NORMAL,
                                                    wx.FONTWEIGHT_NORMAL))
-        fpick = PyFontPicker(self, DocGenPanel.ID_FONT_PICKER, fnt)
+        fpick = ecpickers.PyFontPicker(self, DocGenPanel.ID_FONT_PICKER, fnt)
         fpick.SetToolTipString(_("Sets the main/default font of the document"))
         fnt = Profile_Get('FONT2', 'font', wx.Font(10, wx.FONTFAMILY_SWISS,
                                                    wx.FONTSTYLE_NORMAL,
                                                    wx.FONTWEIGHT_NORMAL))
-        fpick2 = PyFontPicker(self, DocGenPanel.ID_FONT_PICKER2, fnt)
+        fpick2 = ecpickers.PyFontPicker(self, DocGenPanel.ID_FONT_PICKER2, fnt)
         fpick2.SetToolTipString(_("Sets a secondary font used for special "
                                   "regions when syntax highlighting is in use"))
 
@@ -965,7 +966,7 @@ class AppearancePanel(wx.Panel):
 
         # Font
         fnt = Profile_Get('FONT3', 'font', wx.NORMAL_FONT)
-        fpick = PyFontPicker(self, wx.ID_ANY, fnt)
+        fpick = ecpickers.PyFontPicker(self, wx.ID_ANY, fnt)
         fpick.SetToolTipString(_("Main display font for various UI components"))
 
         # Misc
@@ -1893,98 +1894,6 @@ class ExChoice(wx.Choice):
         if val.isalpha():
             val.lower()
         return val
-
-class PyFontPicker(wx.Panel):
-    """A slightly enhanced wx.FontPickerCtrl that displays the choosen font in
-    the text control using the choosen font as well as the font's size using
-    nicer formatting.
-
-    """
-    def __init__(self, parent, id_=wx.ID_ANY, default=wx.NullFont):
-        """Initializes the PyFontPicker
-        @param default: The font to initialize as selected in the control
-
-        """
-        wx.Panel.__init__(self, parent, id_, style=wx.NO_BORDER)
-
-        # Attributes
-        if default == wx.NullFont:
-            self._font = wx.SystemSettings_GetFont(wx.SYS_SYSTEM_FONT)
-        else:
-            self._font = default
-        self._text = wx.TextCtrl(self, style=wx.TE_CENTER | wx.TE_READONLY)
-        self._text.SetFont(default)
-        self._text.SetValue(u"%s - %dpt" % (self._font.GetFaceName(), \
-                                            self._font.GetPointSize()))
-        self._button = wx.Button(self, label=_("Set Font") + u'...')
-
-        # Layout
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.AddMany([(self._text, 1, wx.ALIGN_CENTER_VERTICAL), ((5, 5), 0),
-                       (self._button, 0, wx.ALIGN_CENTER_VERTICAL)])
-        self.SetSizer(sizer)
-        self.SetAutoLayout(True)
-
-        # Event Handlers
-        self.Bind(wx.EVT_BUTTON, lambda evt: self.ShowFontDlg(), self._button)
-        self.Bind(wx.EVT_FONTPICKER_CHANGED, self.OnChange)
-
-    def GetFontValue(self):
-        """Gets the currently choosen font
-        @return: wx.Font
-
-        """
-        return self._font
-
-    def GetTextCtrl(self):
-        """Gets the widgets text control
-        @return: wx.TextCtrl
-
-        """
-        return self._text
-
-    def OnChange(self, evt):
-        """Updates the text control using our custom stylings after
-        the font is changed.
-        @param evt: The event that called this handler
-
-        """
-        font = evt.GetFont()
-        if font.IsNull():
-            return
-        self._font = font
-        self._text.Clear()
-        self._text.SetFont(self._font)
-        self._text.SetValue(u"%s - %dpt" % (font.GetFaceName(), \
-                                            font.GetPointSize()))
-        evt = ed_event.NotificationEvent(ed_event.edEVT_NOTIFY,
-                                         self.GetId(), self._font, self)
-        wx.PostEvent(self.GetParent(), evt)
-
-    def SetButtonLabel(self, label):
-        """Sets the buttons label"""
-        self._button.SetLabel(label)
-        self._button.Refresh()
-
-    def SetToolTipString(self, tip):
-        """Sets the tooltip of the window
-        @param tip: string
-
-        """
-        self._text.SetToolTipString(tip)
-        self._button.SetToolTipString(tip)
-        wx.Panel.SetToolTipString(self, tip)
-
-    def ShowFontDlg(self):
-        """Opens the FontDialog and processes the result"""
-        fdata = wx.FontData()
-        fdata.SetInitialFont(self._font)
-        fdlg = wx.FontDialog(self.GetParent(), fdata)
-        fdlg.ShowModal()
-        fdata = fdlg.GetFontData()
-        fdlg.Destroy()
-        wx.PostEvent(self, wx.FontPickerEvent(self, self.GetId(),
-                                              fdata.GetChosenFont()))
 
 #-----------------------------------------------------------------------------#
 # Utility Functions
