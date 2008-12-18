ebd94b5a77e00cc7382b7f2840a3538c302a5ccb
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2008-12-18 16:37:26 +0000

    Add Enter key to configurable keys in keybinder.

--- wxPython/3rdParty/Editra/src/prefdlg.py
+++ wxPython/3rdParty/Editra/src/prefdlg.py
@@ -1381,7 +1381,7 @@ KEYS = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', '/', ',',
         '.', '[', ']', '{', '}', ':', 'Left', 'Right', 'Down', 'Up', 'Home',
-        'End', 'Tab']
+        'End', 'Enter', 'Tab']
 KEYS.extend(["F" + str(x) for x in range(1, 13)])
 
 if wx.Platform == '__WXMSW__':
