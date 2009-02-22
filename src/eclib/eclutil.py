88c9df9fe46c048561b36e48debc988ddaed7d9a
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2009-02-22 20:58:36 +0000

    move common helper functions into shared module

---
+++ src/eclib/eclutil.py
@@ -0,0 +1,53 @@
+###############################################################################
+# Name: eclutil.py                                                            #
+# Purpose: Common library utilities.                                          #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2009 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
+
+"""
+Editra Control Library: Editra Control Library Utility
+
+Miscellaneous utility functions and gui helpers
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__svnid__ = "$Id$"
+__revision__ = "$Revision$"
+
+__all__ = []
+
+#-----------------------------------------------------------------------------#
+# Imports
+import wx
+
+#-----------------------------------------------------------------------------#
+
+def AdjustAlpha(colour, alpha):
+    """Adjust the alpha of a given colour"""
+    return wx.Colour(colour.Red(), colour.Green(), colour.Blue(), alpha)
+
+def AdjustColour(color, percent, alpha=wx.ALPHA_OPAQUE):
+    """ Brighten/Darken input colour by percent and adjust alpha
+    channel if needed. Returns the modified color.
+    @param color: color object to adjust
+    @type color: wx.Color
+    @param percent: percent to adjust +(brighten) or -(darken)
+    @type percent: int
+    @keyword alpha: amount to adjust alpha channel
+
+    """
+    radj, gadj, badj = [ int(val * (abs(percent) / 100.))
+                         for val in color.Get() ]
+
+    if percent < 0:
+        radj, gadj, badj = [ val * -1 for val in [radj, gadj, badj] ]
+    else:
+        radj, gadj, badj = [ val or 255 for val in [radj, gadj, badj] ]
+
+    red = min(color.Red() + radj, 255)
+    green = min(color.Green() + gadj, 255)
+    blue = min(color.Blue() + badj, 255)
+    return wx.Colour(red, green, blue, alpha)
