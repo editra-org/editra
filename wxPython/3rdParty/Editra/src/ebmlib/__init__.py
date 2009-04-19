84c2d4b22be6f935d0b3540c20dc09f7abad7475
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2009-04-19 05:42:42 +0000

    Start reorganization of low level non-gui classes into separate sub package.

---
+++ wxPython/3rdParty/Editra/src/ebmlib/__init__.py
@@ -0,0 +1,21 @@
+###############################################################################
+# Name: __init__.py                                                           #
+# Purpose: Editra Buisness Model Library                                      #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2009 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
+
+"""
+Editra Buisness Model Library:
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__cvsid__ = "$Id$"
+__revision__ = "$Revision$"
+
+#-----------------------------------------------------------------------------#
+
+from searcheng import *
+from fchecker import *
