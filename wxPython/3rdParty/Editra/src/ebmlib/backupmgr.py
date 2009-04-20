4805d0f667341c3556ba107879e4ad88e68e0465
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2009-04-20 04:12:49 +0000

    Add basic file backup helper class

---
+++ wxPython/3rdParty/Editra/src/ebmlib/backupmgr.py
@@ -0,0 +1,87 @@
+###############################################################################
+# Name: backupmgr.py                                                          #
+# Purpose: File Backup Manager                                                #
+# Author: Cody Precord <cprecord@editra.org>                                  #
+# Copyright: (c) 2009 Cody Precord <staff@editra.org>                         #
+# Licence: wxWindows Licence                                                  #
+###############################################################################
+
+"""
+Editra Buisness Model Library: FileBackupMgr
+
+Helper class for managing and creating backups of files.
+
+"""
+
+__author__ = "Cody Precord <cprecord@editra.org>"
+__cvsid__ = "$Id$"
+__revision__ = "$Revision$"
+
+__all__ = [ 'FileBackupMgr', ]
+
+#-----------------------------------------------------------------------------#
+# Imports
+import os
+import shutil
+
+# Locak Imports
+import fileutil
+
+#-----------------------------------------------------------------------------#
+
+class FileBackupMgr(object):
+    """File backup creator and manager"""
+    def __init__(self):
+        object.__init__(self)
+
+        # Attributes
+        
+
+    @staticmethod
+    def GetBackupFilename(self, fname):
+        """Get the unique name for the files backup copy
+        @param fname: string (file path)
+        @return: string
+
+        """
+        return u"%s~" % fname
+
+    def HasBackup(self, fname):
+        """Check if a given file has a backup file available or not
+        @param fname: string (file path)
+
+        """
+        backup = self.GetBackupFilename(fname)
+        return os.path.exists(backup)
+
+    def IsBackupNewer(self, fname):
+        """Is the backup of this file newer than the saved version
+        of the file?
+        @param fname: string (file path)
+        @return: bool
+
+        """
+        backup = self.GetBackupFilename(fname)
+        if os.path.exists(fname) and os.path.exists(backup):
+            mod1 = fileutil.GetFileModTime(backup)
+            mod2 = fileutil.GetFileModTime(fname)
+            return mod1 > mod2
+        else:
+            return False
+
+    def MakeBackup(self, fname):
+        """Create a backup copy of the given filename
+        @param fname: string (file path)
+        @return: bool (True == Success)
+
+        """
+        backup = self.GetBackupFilename(fname)
+        try:
+            if os.path.exists(backup):
+                os.remove(backup)
+
+            shutil.copy2(fname, backup)
+        except:
+            return False
+        else:
+            return True
