38c1ec07463a2c9248d3810894ebdbdbf18d88e8
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2010-01-19 03:59:18 +0000

    wxPython 2.9 compatibility changes NullColor -> NullColour

--- src/eclib/platebtn.py
+++ src/eclib/platebtn.py
@@ -557,7 +557,7 @@ class PlateButton(wx.PyControl):
         super(PlateButton, self).SetLabel(label)
         self.InvalidateBestSize()
 
-    def SetLabelColor(self, normal, hlight=wx.NullColor):
+    def SetLabelColor(self, normal, hlight=wx.NullColour):
         """Set the color of the label. The optimal label color is usually
         automatically selected depending on the button color. In some
         cases the colors that are choosen may not be optimal.
