0a8bb2d74360cc4b374b8baf18de28e2493a0f63
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2010-03-30 01:16:28 +0000

    Fix error that could occur when iterating over syntax globals during startup. *Caused by async code modifying global dictionary which caused its size to change. Make copy of dict before iterating over it.

--- wxPython/3rdParty/Editra/src/syntax/synglob.py
+++ wxPython/3rdParty/Editra/src/syntax/synglob.py
@@ -137,9 +137,11 @@ def GetDescriptionFromId(lang_id):
 
     """
     rval = LANG_TXT
-    for key, val in globals().iteritems():
+    # Guard against async code that may be modifying globals
+    globs = dict(globals())
+    for key, val in globs.iteritems():
         if val == lang_id and key.startswith('ID_LANG'):
-            rval = globals().get(key[3:], LANG_TXT)
+            rval = globs.get(key[3:], LANG_TXT)
             break
     return rval
 
@@ -152,9 +154,11 @@ def GetIdFromDescription(desc):
     """
     rval = ID_LANG_TXT
     desc = desc.lower()
-    for key, val in globals().iteritems():
+    # Guard against async code that may be modifying globals
+    globs = dict(globals())
+    for key, val in globs.iteritems():
         if isinstance(val, basestring) and \
            val.lower() == desc and key.startswith('LANG_'):
-            rval = globals().get(u"ID_" + key, ID_LANG_TXT)
+            rval = globs.get(u"ID_" + key, ID_LANG_TXT)
             break
     return rval
