aec1f821ddb19a9730956697157efcf3dc264735
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2010-05-16 19:20:00 +0000

    Bump version number for 0.5.60 release

--- wxPython/3rdParty/Editra/src/info.py
+++ wxPython/3rdParty/Editra/src/info.py
@@ -15,7 +15,7 @@ __revision__ = "$Revision$"
 #-----------------------------------------------------------------------------#
 
 AUTHOR = 'Cody Precord'
-VERSION = '0.5.52'
+VERSION = '0.5.60'
 PROG_NAME = 'Editra'
 HOME_PAGE = "http://editra.org"
 I18N_PAGE = "https://translations.launchpad.net/editra/trunk/+pots/editra"
