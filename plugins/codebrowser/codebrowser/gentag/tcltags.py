c8df307153d4194f315c6d2e5242a594964ee46a
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2010-06-05 22:03:14 +0000

    Add support for snit syntax to codebrowser for tcl/tk - support scoped methods/typemethods - support typevariable/variable - support packages

--- plugins/codebrowser/codebrowser/gentag/tcltags.py
+++ plugins/codebrowser/codebrowser/gentag/tcltags.py
@@ -34,12 +34,47 @@ def GenerateTags(buff):
     """
     rtags = taglib.DocStruct()
     rtags.SetElementDescription('procedure', "Procedure Definitions")
+    rtags.SetElementDescription('class', "SNIT")
+
+    # Scope tracking for SNIT blocks
+    insnit = False
+    cursnit = None
+    openparens = 0
 
     for lnum, line in enumerate(buff):
         line = line.strip()
 
         # Skip comment and empty lines
         if line.startswith(u"#") or not line:
+            continue
+
+        if insnit and cursnit is not None:
+            if parselib.IsToken(line, 0, u'method') or \
+               parselib.IsToken(line, 0, u'typemethod'):
+                parts = line.split()
+                if len(parts) > 1:
+                    name = parts[1]
+                    cursnit.AddMethod(taglib.Method(name, lnum))
+            elif parselib.IsToken(line, 0, u'typevariable') or \
+                 parselib.IsToken(line, 0, u'variable'):
+                parts = line.split()
+                if len(parts) > 1:
+                    name = parts[1]
+                    cursnit.AddVariable(taglib.Variable(name, lnum))
+            elif parselib.IsToken(u'constructor', 0, line) or \
+                 parselib.IsToken(u'destructor', 0, line):
+                 name = parselib.GetFirstIdentifier(line)
+                 cursnit.AddMethod(taglib.Method(name, lnum))
+            elif parselib.IsToken(line, 0, u"package"):
+                 pkg = GetPackage(line, lnum)
+                 if pkg:
+                     cursnit.AddElement('package', pkg)
+
+            # Update Scope
+            openparens += GetParenCount(line)
+            if openparens == 0:
+                insnit = False
+                cursnit = None
             continue
 
         # Check for Procedure defs
@@ -56,8 +91,42 @@ def GenerateTags(buff):
                     space_l.AddElement('procedure', taglib.Procedure(spaces[-1], lnum))
                 else:
                     rtags.AddElement('procedure', taglib.Procedure(parts[1], lnum))
+        elif line.startswith(u'snit::'):
+            parts = line.split()
+            if len(parts) > 1:
+                insnit = True
+                openparens = GetParenCount(line)
+                name = parts[1]
+                cursnit = taglib.Class(name, lnum)
+                rtags.AddClass(cursnit)
+        elif parselib.IsToken(line, 0, u"package"):
+             pkg = GetPackage(line, lnum)
+             if pkg:
+                 rtags.AddElement('package', pkg)
 
     return rtags
+
+
+# Helper functions
+
+def GetParenCount(line):
+    """Get the delta of parens on the given line"""
+    openparens = line.count(u"{")
+    openparens -= line.count(u"}")
+    return openparens
+
+def GetPackage(line, lnum):
+    """Get a package object from the current line
+    @precondition: line has a package def on it
+
+    """
+    package = None
+    parts = line.split()
+    # package require name version
+    if len(parts) >= 3:
+        name = u" ".join(parts[2:])
+        package = taglib.Package(name, lnum)
+    return package
 
 #-----------------------------------------------------------------------------#
 # Test
