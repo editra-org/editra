94154d254104c51c66896081eea485ec8a97ada5
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2010-06-15 04:00:50 +0000

    Lint

--- src/syntax/_python.py
+++ src/syntax/_python.py
@@ -113,7 +113,6 @@ def AutoIndenter(estc, pos, ichar):
     line = estc.GetCurrentLine()
     spos = estc.PositionFromLine(line)
     text = estc.GetTextRange(spos, pos)
-    epos = estc.GetLineEndPosition(line)
     eolch = estc.GetEOLChar()
     inspace = text.isspace()
 
