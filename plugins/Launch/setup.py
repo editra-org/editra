c6fb747825c8c759b9a1ae2cd861e8ed81a7b0fa
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2010-12-26 16:11:56 +0000

    Ensure that the process exit status is output to a new line and not incidentally appended to the previous output line.

--- plugins/Launch/setup.py
+++ plugins/Launch/setup.py
@@ -16,7 +16,7 @@ except ImportError:
 if setup != None:
     setup(
         name='Launch',
-        version='1.8',
+        version='1.9',
         description=__doc__,
         author=__author__,
         author_email="cprecord@editra.org",
