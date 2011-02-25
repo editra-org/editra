ec9939f5735a0dbd3ce7b8149810381a02601db9
CJP <CJP@c3d73ce0-8a6f-49c7-b76d-6d57e0e08775>
2011-02-25 21:04:49 +0000

    Tab control now uses user Display Font setting for setting its Font

--- src/ed_pages.py
+++ src/ed_pages.py
@@ -105,6 +105,7 @@ class EdPages(aui.AuiNotebook):
         # Set custom options
         self.SetSashDClickUnsplit(True)
         self.SetMinMaxTabWidth(125, 135)
+        self.UpdateFontSetting()
 
         # Notebook Events
         self.Bind(wx.EVT_WINDOW_DESTROY, self.OnDestroy, self)
@@ -131,6 +132,8 @@ class EdPages(aui.AuiNotebook):
         ed_msg.Subscribe(self.OnUpdatePosCache, ed_msg.EDMSG_UI_STC_POS_JUMPED)
         ed_msg.RegisterCallback(self.OnDocPointerRequest,
                                 ed_msg.EDREQ_DOCPOINTER)
+        ed_msg.Subscribe(self.OnConfigMsg,
+                         ed_msg.EDMSG_PROFILE_CHANGE + ('FONT3',))
 
         # Add a blank page
         self.NewPage()
@@ -142,6 +145,7 @@ class EdPages(aui.AuiNotebook):
         if evt.GetId() == self.GetId():
             ed_msg.Unsubscribe(self.OnThemeChanged)
             ed_msg.Unsubscribe(self.OnUpdatePosCache)
+            ed_msg.Unsubscribe(self.OnConfigMsg)
             ed_msg.UnRegisterCallback(self.OnDocPointerRequest)
 
     #---- Function Definitions ----#
@@ -583,6 +587,19 @@ class EdPages(aui.AuiNotebook):
 
         """
         self.UpdateAllImages()
+
+    def OnConfigMsg(self, msg):
+        """Handle configuration change updates"""
+        mtype = msg.GetType()[-1]
+        if mtype == 'FONT3':
+            # UI display font has changed
+            self.UpdateFontSetting()
+
+    def UpdateFontSetting(self):
+        """Update font setting using latest profile data"""
+        font = Profile_Get('FONT3', 'font', None)
+        if font:
+            self.SetFont(font)
 
     def OnUpdatePosCache(self, msg):
         """Update the position cache for buffer position changes
--- src/prefdlg.py
+++ src/prefdlg.py
@@ -1310,6 +1310,8 @@ class AppearancePanel(wx.Panel, PreferencesPanelBase):
         font = evt.GetValue()
         if isinstance(font, wx.Font) and not font.IsNull():
             Profile_Set('FONT3', font, 'font')
+            # TODO: this callback is no longer necessary due to the
+            #       profile notifier callbacks.
             ed_msg.PostMessage(ed_msg.EDMSG_DSP_FONT, font)
 
     @staticmethod
